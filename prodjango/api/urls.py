a62962248b8035f8aab8f427b1e9932b7a16c056
turiii1 <arturituri88@gmail.com>
2024-04-18 16:54:30 +0200

    koment3

--- prodjango/api/urls.py
+++ prodjango/api/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name="homePage"),
     path('about/',views.about, name="aboutPage"),
-    path('contact/',views.contact, name="ctaboutPage")
+    path('contact/',views.contact, name="ctaboutPage"),
+    path('preview/<id>',views.details, name="details"),
+    path('show/<id>',views.services,name="showpage"),
 ]
 
--- prodjango/api/views.py
+++ prodjango/api/views.py
@@ -3,13 +3,39 @@ from .models import *
 
 # Create your views here.
 def home(request):
+    reviews=Review.objects.all()
+    shows=Category.objects.all()
     items=Item.objects.all()
-    context={"items":items}
+    context={"items":items,"shows":shows,"reviews":reviews }
     return  render(request,'home.html',context)   
 
 def about(request):
-    return  render(request,'about.html')   
+    shows=Category.objects.all()
+    context={"shows":shows }
+    return  render(request,'about.html',context)   
 
 def contact(request):
-    return  render(request,'contact.html')   
+    shows=Category.objects.all()
+    if request.method== "POST":
+        email_info = request.POST["email"]
+        comment_info = request.POST["comment"]
+        name_info=request.POST["name"]
+        surname_info=request.POST["surname"]
+
+        Contact(email=email_info,comment=comment_info,name=name_info,surname=surname_info).save()
+    context={"shows":shows }
+    return  render(request,'contact.html',context)   
+
+def details(request,id):
+    shows=Category.objects.all()
+    item=Item.objects.get(pk=id)
+    context={"item":item,"shows":shows}
+    return render(request,'detailitem.html',context)
+
+def services(request,id):
+    elementCategory=Item.objects.filter(item_category=Item)
+    show=Category.objects.get(pk=id)
+    context={"show":show,"elementCategory":elementCategory}
+    return render(request,'custom.html',context)
+
 
